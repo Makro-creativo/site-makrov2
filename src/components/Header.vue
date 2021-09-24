bbcf94bac5d6f0cf3aadedb1bad062c26c8508c5
PaulMaldonado <paul_pastore10@hotmail.com>
2021-09-24 11:59:59 -0500

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="header">
+      <section class="section-header py-7 d-flex">
+          <div class="container align-self-center">
+              <div class="row">
+                  <div class="col-lg-7 d-flex">
+                      <div class="align-self-center">
+                          <h1 class="display-4 text-dark font-weight-bold mb-3">
+                              Impulso para <span class="text-orange">crecer.</span>
+                          </h1>
+                          <p class="text-muted mb-4">
+                              Creamos la página que tú marca necesita.
+                          </p>
+                          <div class="btn btn-dark">Nuestro portafolio</div>
+                      </div>
+                  </div>
+
+                  <div class="col-lg-5">
+                    <img src="../assets/img/header-image.svg" alt="Header image" class="img-fluid mb-3">
+                  </div>
+              </div>
+          </div>
+      </section>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#FDF3E5" fill-opacity="1" d="M0,160L48,176C96,192,192,224,288,213.3C384,203,480,149,576,112C672,75,768,53,864,80C960,107,1056,181,1152,192C1248,203,1344,149,1392,122.7L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+      </svg>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+    .py-7 {
+        padding-top: 10rem;
+        padding-bottom: 1rem;
+    }
+
+    .section-header {
+        background-color: #FFFFFF;
+    }
+
+    .section-header h1 {
+        font-family: 'Poppins', sans-serif;
+        font-weight: bold;
+    }
+
+    .text-orange {
+        font-family: 'Poppins', sans-serif;
+        color: #FABB6E;
+        font-weight: bold;
+    }
+
+    .btn-dark {
+        background-color: #FABB6E !important;
+        border-color: #FABB6E !important;
+        padding: 1rem;
+        border-radius: 15px;
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="navbar">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow p-2 fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="../assets/img/logo_makro_creativo_web_color.svg" alt="" width="120" height="100">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="icon ion-md-menu"></i>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/">Inicio</router-link>
+                    </li>
+
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/nosotros">Nosotros</router-link>
+                    </li>
+
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/branding">Branding</router-link>
+                    </li>
+
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/servicios">Servicios</router-link>
+                    </li>
+
+                    <li class="nav-item">
+                        <router-link class="btn btn-outline-primary" to="/contacto">Contacto</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .bg-light {
+        background-color: #FFFFFF !important;
+        font-family: 'Poppins', sans-serif;
+    }
+
+    .ion-md-menu {
+        font-size: 1.5em;
+    }
+
+    .nav-link {
+        color: #000000 !important;
+        font-size: 1.2em;
+        font-weight: 400;
+        padding: .8rem 1rem !important;
+        font-family: 'Poppins', sans-serif;
+    }
+
+    .btn-outline-primary {
+        border-color: #FABB6E !important;
+        color: #FABB6E;
+        border-radius: 20px;
+        padding: .6rem 1rem !important;
+        font-weight: 400;
+        font-family: 'Poppins', sans-serif;
+    }
+
+    .btn-outline-primary:hover {
+        background-color: #FABB6E !important;
+        color: #FFFFFF;
+        font-weight: 400;
+        font-family: 'Poppins', sans-serif;
+    }
+</style>
