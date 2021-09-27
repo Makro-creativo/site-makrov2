8dd5e164d7baa53b1147f9635792531eb1307b1f
PaulMaldonado <paul_pastore10@hotmail.com>
2021-09-27 15:49:43 -0500

    Se agrego el componente Contact y se agregaron 3 gif

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -5,16 +5,41 @@
 
           <div class="container">
               <div class="row g-0">
-                  <div class="col-md-4 col-sm-12 col-lg-4 col-xl-4 col-xxl-4">
-
+                  <div class="col-md-4 col-sm-12 col-lg-4 col-xl-4 col-xxl-4 d-flex align-items-stretch">
+                      <div class="card shadow-lg p-3 mb-5 bg-body rounded">
+                          <img src="../assets/img/ecommerce-icon.gif" alt="Ecommerce servicios" class="img-fluid card-img-top image-ecommerce">
+                          <div class="card-body">
+                              <p class="card-text">
+                                  Haga crecer su empresa de comercio electrónico y permítanos ayudarlo a atraer y convertir a más clientes.
+                              </p>
+                          </div>
+                      </div>
                   </div>
 
-                  <div class="col-md-4 col-sm-12 col-lg-4 col-xl-4 col-xxl-4">
-                      
+                  <div class="col-md-4 col-sm-12 col-lg-4 col-xl-4 col-xxl-4 d-flex align-items-stretch">
+                    <div class="card shadow-lg p-3 mb-5 bg-body rounded">
+                        <img src="../assets/img/design-web.gif" alt="Diseño web" class="img-fluid card-img-top image-design">
+
+                        <div class="card-body">
+                            <p class="card-text">
+                                Desarrollo y diseño Web a la medida, en Makro Creativo diseñamos Sitios Web 
+                                para tú negocio, desde Landing page, sitios ecommerce y ERP. Ven y te cotizamos.
+                            </p>
+                        </div>
+                    </div>
                   </div>
 
-                  <div class="col-md-4 col-sm-12 col-lg-4 col-xl-4 col-xxl-4">
-                      
+                  <div class="col-md-4 col-sm-12 col-lg-4 col-xl-4 col-xxl-4 d-flex align-items-stretch">
+                      <div class="card shadow-lg p-3 mb-5 bg-body rounded">
+                          <img src="../assets/img/video_conference.gif" alt="Video conferencias" class="img-fluid card-img-top image-conference">
+
+                          <div class="card-bdy">
+                              <p class="card-text">
+                                  Desarrollamos talleres para los empresarios que van empezando para generarles una 
+                                  mejor estrategía de Marketing y de ventas en linea.
+                              </p>
+                          </div>
+                      </div>
                   </div>
               </div>
           </div>
@@ -38,5 +63,24 @@ export default {
         font-weight: bold;
         font-family: 'Poppins', sans-serif;
         line-height: 58px;
+    }
+
+    .card {
+        border: transparent !important;
+    }
+
+    .image-ecommerce {
+        width: 200px !important;
+        height: 200px !important;
+    }
+
+    .image-design {
+        width: 200px !important;
+        height: 200px !important;
+    }
+
+    .image-conference {
+        width: 200px !important;
+        height: 200px !important;
     }
 </style>>
