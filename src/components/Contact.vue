5eff6b1497b1046b04ed14ae1f6c6df925025172
PaulMaldonado <paul_pastore10@hotmail.com>
2021-10-06 13:59:12 -0500

    Se modifico el componente Contact y el index.html, también se agrego icono de whatsapp

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -84,7 +84,11 @@
               </div>
           </div>
       </section>
-  </div>
+
+    <a href="https://api.whatsapp.com/send?phone=523331498145&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20sus%20servicios." class="float" target="_blank">
+        <i class="fab fa-whatsapp my-float"></i>
+    </a>
+    </div>
 </template>
 
 <script>
@@ -208,5 +212,25 @@ export default {
 
     .list-group-item {
         border: transparent !important;
+    }
+
+
+    .float{
+        position:fixed;
+        width:60px;
+        height:60px;
+        bottom:40px;
+        right:40px;
+        background-color:#25d366;
+        color:#FFF;
+        border-radius:50px;
+        text-align:center;
+        font-size:30px;
+        box-shadow: 2px 2px 3px #999;
+        z-index:100;
+    }
+
+    .my-float{
+        margin-top:16px;
     }
 </style>
